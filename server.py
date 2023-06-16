d151739791e9d8435f1d8550d9aebea570219028
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2023-06-17 01:40:19 +0700

    venv -m

---
+++ server.py
@@ -0,0 +1,12 @@
+import socket
+import threading
+
+
+#Fungsi utama 
+
+def main():
+    # Membuat server socker dengan class object
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+if __name__ == '__main__':
+    main()
