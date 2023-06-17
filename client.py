d4b0051e9a6d426d740c8495874ff165148026ee
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2023-06-18 01:16:58 +0700

    gocha

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket
 import threading
-import tkinter as tk 
+import tkinter as tk
+from tkinter import scrolledtext
+from tkinter import messagebox
 
 DARK_GREY = '#121212'
 MEDIUM_GREY = '#1F1B24'
@@ -10,11 +12,41 @@ FONT = ("Helvetica", 17)
 BUTTON_FONT = ('Helvetica', 15)
 SMALL_FONT = ("Helvetica", 13)
 
+def add_pesan(pesan):
+    latar_pesan.config(state=tk.NORMAL)
+    latar_pesan.insert(tk.END, pesan + '\n')
+    latar_pesan.config(state=tk.DISABLED)
+
 def connect():
-    print('Tombol ini berfungsi')
+    try:
+        # CONNECT KE SERVER
+        global client  # Menandai variabel sebagai global
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((HOST, PORT))
+        print('Berhasil terhubung ke server')
+        add_pesan('[SERVER] Berhasil terhubung ke server')
+    except:
+        messagebox.showerror("Tidak dapat terhubung ke server", f"Tidak dapat terhubung ke server {HOST} {PORT}")
+
+    username = username_textbox.get()
+    if username != '':
+        client.sendall(username.encode())
+    else:
+        messagebox.showerror('Username tidak valid', 'Username tidak boleh kosong')
+
+    threading.Thread(target=listen_pesan_dari_server, args=(client,)).start()
+
+    username_textbox.config(state=tk.DISABLED)
+    tombol_username.config(state=tk.DISABLED)
 
 def kirim_pesan():
-    print("kirim pesan")
+    pesan = pesan_textbox.get()
+    if pesan != '':
+        client.sendall(pesan.encode())
+        latar_pesan.delete(0, len(pesan))
+
+    else:
+        messagebox.showerror("Pesan kosong", "Pesan tidak boleh kosong")
 
 root = tk.Tk()
 root.geometry("600x600")
@@ -41,6 +73,7 @@ username_textbox = tk.Entry(top_frame, font=SMALL_FONT, bg=MEDIUM_GREY, fg=WHITE
 username_textbox.pack(side=tk.LEFT, padx=(0, 5), fill=tk.X, expand=True)
 
 tombol_username = tk.Button(top_frame, text='Masuk', font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=connect, width=10)
+tombol_username.config(font=("Helvetica", 13))
 tombol_username.pack(side=tk.LEFT)
 
 pesan_textbox = tk.Entry(buttom_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=38)
@@ -48,6 +81,10 @@ pesan_textbox.pack(side=tk.LEFT, padx=10)
 
 tombol_pesan = tk.Button(buttom_frame, text='Kirim', font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=kirim_pesan)
 tombol_pesan.pack(side=tk.LEFT, padx=10)
+
+latar_pesan = scrolledtext.ScrolledText(middle_frame, font=SMALL_FONT, bg=MEDIUM_GREY, fg=WHITE, width=67, height=23)
+latar_pesan.config(state=tk.DISABLED)
+latar_pesan.pack(side=tk.TOP)
 
 HOST = '127.0.0.1'
 PORT = 1234
@@ -58,46 +95,12 @@ def listen_pesan_dari_server(client):
         if pesan != '':
             username = pesan.split("-")[0]
             isipesan = pesan.split('-')[1]
-            print(f'[{username}] {isipesan}')
+            add_pesan(f'[{username}] {isipesan}')
         else:
-            print('Pesan kosong diterima dari server')
-
-def kirim_pesan_ke_server(client):
-    while True:
-        pesan = input('Pesan: ')
-        if pesan != '':
-            client.sendall(pesan.encode())
-        else:
-            print("Pesan kosong")
-            exit(0)
-
-def connection_to_server(client):
-    username = input('Masukkan username: ')
-    if username != '':
-        client.sendall(username.encode())
-    else:
-        print('Username tidak boleh kosong')
-        exit(0)
-
-    threading.Thread(target=listen_pesan_dari_server, args=(client,)).start()
-    kirim_pesan_ke_server(client)
+            messagebox.showerror('Error', 'Pesan kosong diterima dari server')
 
 def main():
-
     root.mainloop()
-
-
-    #membuat object socket
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    try:
-        client.connect((HOST, PORT))
-        print('Berhasil terhubung ke server')
-    except Exception as e:
-        print(f"Tidak dapat terhubung ke server {HOST} {PORT}: {str(e)}")
-        return
-
-    connection_to_server(client)
 
 if __name__ == '__main__':
     main()
