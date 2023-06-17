5abb616aaf95da7e549479d9b10ba6f8179d8c20
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2023-06-17 12:55:23 +0700

    Snap, something went wrong while displaying

--- client.py
+++ client.py
@@ -4,6 +4,44 @@ import threading
 HOST = '127.0.0.1'
 PORT = 1234
 
+def listen_pesan_dari_server(client):
+
+    while 1:
+
+        pesan = client.recv(2048).decode('utf-8')
+        if pesan != '':
+            username = pesan.split("-")[0]
+            isipesan = pesan.split('-')[1]
+
+            print(f'[{username}] {isipesan}')
+        else:
+            print('Pesan kosong diterima dari client')
+
+def kirim_pesan_ke_server(client):
+
+    while 1:
+
+        username = input('Pesan : ')
+        if pesan != '':
+            client.kirimsemua(username.encode())
+        else:
+            print("pesan kosong")
+            exit(0)
+
+        kirim_pesan_ke_server(client)
+#koneksi dengan server
+def connection_to_server(client):
+
+    username = input('Masukan username: ')
+    if username != '':
+        client.kirimsemua(username.encode())
+    else:
+        print('Username tidak boleh kosong')
+        exit(0)
+
+    threading.Thread(target=listen_pesan_dari_server, args=(client, )).start()
+
+
 def main():
     pass
 
@@ -16,5 +54,8 @@ def main():
         print(f'Berhasil terhubung ke server')
     except:
         print(f"Tidak dapat connect ke server{HOST} {PORT}")
+
+    connection_to_server(client)
+
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -4,6 +4,49 @@ import threading
 HOST = '127.0.0.1'
 PORT = 1234  # Silahkan sesuaikan port 0 sampai 65535
 LISTENER_LIMIT = 5
+client_aktif = [] # jumlah keseluruhan client aktif
+
+
+# Merespon pesan yang akan datang
+def pesan(client, username):
+
+    while 1: 
+        pesan = client.recv(2048).decode('utf-8')
+        if pesan !='':
+
+            isi_pesan = username + '-' + pesan
+            kirim_semua_pesan((isi_pesan))
+
+        else:
+            print(f'Pesan kosong dikirim dari {username} client')
+
+# mengirim pesan ke satu client
+
+def kirim_pesan(client, pesan):
+
+    client.kirimsemua(pesan.encode())
+
+# untuk mengirimkan semua pesan ke client apabila terhubung dengan server 
+def kirim_semua_pesan(from_username, pesan):
+    
+    for user in client_aktif:  
+
+        kirim_pesan(user[1], pesan)
+
+# handle client
+def client_hand(client):
+
+    # Server akan merespon pesan yang dikirim
+    # Sesuai dengan username 
+    while 1: 
+
+        username = client.recv(2048).decode('utf-8')
+        if username != '':
+            client_aktif.append(username, client)
+            break
+        else:
+            print('Terdapat kendala')
+    threading.Thread(target=pesan, args=(client, username, )).start()
 
 # Fungsi utama
 def main():
@@ -28,5 +71,6 @@ def main():
         client, address = server.accept()
         print(f'Sukses terhubung dengan client {address[0]} {address[1]}')
 
+        threading.Thread(target=client_hand, args=(client, )).start()
 if __name__ == '__main__':
     main()
