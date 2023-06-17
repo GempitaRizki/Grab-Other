0d4bed797ac452caf2d21a64c8da733b2f745e7d
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2023-06-17 13:13:53 +0700

    well done

--- client.py
+++ client.py
@@ -1,59 +1,48 @@
-import socket 
+import socket
 import threading
 
 HOST = '127.0.0.1'
 PORT = 1234
 
 def listen_pesan_dari_server(client):
-
-    while 1:
-
+    while True:
         pesan = client.recv(2048).decode('utf-8')
         if pesan != '':
             username = pesan.split("-")[0]
             isipesan = pesan.split('-')[1]
-
             print(f'[{username}] {isipesan}')
         else:
-            print('Pesan kosong diterima dari client')
+            print('Pesan kosong diterima dari server')
 
 def kirim_pesan_ke_server(client):
-
-    while 1:
-
-        username = input('Pesan : ')
+    while True:
+        pesan = input('Pesan: ')
         if pesan != '':
-            client.kirimsemua(username.encode())
+            client.sendall(pesan.encode())
         else:
-            print("pesan kosong")
+            print("Pesan kosong")
             exit(0)
 
-        kirim_pesan_ke_server(client)
-#koneksi dengan server
 def connection_to_server(client):
-
-    username = input('Masukan username: ')
+    username = input('Masukkan username: ')
     if username != '':
-        client.kirimsemua(username.encode())
+        client.sendall(username.encode())
     else:
         print('Username tidak boleh kosong')
         exit(0)
 
-    threading.Thread(target=listen_pesan_dari_server, args=(client, )).start()
-
+    threading.Thread(target=listen_pesan_dari_server, args=(client,)).start()
+    kirim_pesan_ke_server(client)
 
 def main():
-    pass
-
-    #membuat socket object 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    #connect ke server 
     try:
         client.connect((HOST, PORT))
-        print(f'Berhasil terhubung ke server')
-    except:
-        print(f"Tidak dapat connect ke server{HOST} {PORT}")
+        print('Berhasil terhubung ke server')
+    except Exception as e:
+        print(f"Tidak dapat terhubung ke server {HOST} {PORT}: {str(e)}")
+        return
 
     connection_to_server(client)
 
--- server.py
+++ server.py
@@ -2,75 +2,71 @@ import socket
 import threading
 
 HOST = '127.0.0.1'
-PORT = 1234  # Silahkan sesuaikan port 0 sampai 65535
+PORT = 1234
 LISTENER_LIMIT = 5
-client_aktif = [] # jumlah keseluruhan client aktif
-
+client_aktif = []  # Jumlah keseluruhan client aktif
 
 # Merespon pesan yang akan datang
 def pesan(client, username):
-
-    while 1: 
-        pesan = client.recv(2048).decode('utf-8')
-        if pesan !='':
-
-            isi_pesan = username + '-' + pesan
-            kirim_semua_pesan((isi_pesan))
-
-        else:
-            print(f'Pesan kosong dikirim dari {username} client')
-
-# mengirim pesan ke satu client
-
-def kirim_pesan(client, pesan):
-
-    client.kirimsemua(pesan.encode())
-
-# untuk mengirimkan semua pesan ke client apabila terhubung dengan server 
-def kirim_semua_pesan(from_username, pesan):
-    
-    for user in client_aktif:  
-
-        kirim_pesan(user[1], pesan)
-
-# handle client
-def client_hand(client):
-
-    # Server akan merespon pesan yang dikirim
-    # Sesuai dengan username 
-    while 1: 
-
-        username = client.recv(2048).decode('utf-8')
-        if username != '':
-            client_aktif.append(username, client)
+    while True:
+        try:
+            pesan = client.recv(2048).decode('utf-8')
+            if pesan != '':
+                isi_pesan = username + '-' + pesan
+                kirim_semua_pesan(username, isi_pesan)
+            else:
+                print(f'Pesan kosong dikirim dari {username} client')
+        except ConnectionResetError:
+            print(f'Koneksi dengan {username} ditutup secara paksa.')
+            client_aktif.remove((username, client))
             break
-        else:
-            print('Terdapat kendala')
-    threading.Thread(target=pesan, args=(client, username, )).start()
+
+# Mengirim pesan ke satu client
+def kirim_pesan(client, pesan):
+    client.sendall(pesan.encode())
+
+# Mengirim semua pesan ke client yang terhubung dengan server
+def kirim_semua_pesan(from_username, pesan):
+    for user in client_aktif:
+        try:
+            kirim_pesan(user[1], pesan)
+        except ConnectionResetError:
+            print(f'Koneksi dengan {user[0]} ditutup secara paksa.')
+            client_aktif.remove(user)
+
+# Handle client
+def client_hand(client):
+    while True:
+        try:
+            username = client.recv(2048).decode('utf-8')
+            if username != '':
+                client_aktif.append((username, client))
+                break
+            else:
+                print('Terdapat kendala')
+        except ConnectionResetError:
+            print('Koneksi ditutup secara paksa.')
+            break
+    threading.Thread(target=pesan, args=(client, username,)).start()
 
 # Fungsi utama
 def main():
-    # Membuat server socket dengan class object
-    # AF_INET: kita gunakan untuk alamat IPv4
-    # SOCK_STREAM: kita gunakan untuk menggunkan TCP packets untuk komunikasi
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        # Host IP and PORT
         server.bind((HOST, PORT))
         print(f'Server berjalan pada {HOST} {PORT}')
-    except:
-        print(f'Tidak dapat meraih host {HOST} dan port {PORT}')
+    except Exception as e:
+        print(f'Tidak dapat meraih host {HOST} dan port {PORT}: {str(e)}')
         return
 
-    # Pengaturan limit server
     server.listen(LISTENER_LIMIT)
 
-    # perulangan untuk tetap terhubung dengan koneksi ke client.py
-    while 1:
+    while True:
         client, address = server.accept()
         print(f'Sukses terhubung dengan client {address[0]} {address[1]}')
 
-        threading.Thread(target=client_hand, args=(client, )).start()
+        threading.Thread(target=client_hand, args=(client,)).start()
+
 if __name__ == '__main__':
     main()
